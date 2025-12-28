b9166eb3c30463699222dd66e1f14cb9eb838ab2
naseffipaid <senafirdissa58@gmail.com>
2025-12-28 09:00:42 -0800

    creack detection and water quality trained models

---
+++ backend/app/main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, UploadFile, File
+from tensorflow.keras.models import load_model
+from PIL import Image
+import numpy as np
+import io
+import joblib
+
+app = FastAPI()
+model = load_model("saved_models/crack_classifier")
+water_quality_model = load_model("saved_models/water_quality_model")
+scaler = joblib.load("saved_models/water_scaler.pkl") 
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    img = Image.open(io.BytesIO(await file.read())).resize((224,224))
+    img = np.array(img)/255.0
+    img = img.reshape(1,224,224,3)
+
+    pred = model.predict(img)[0][0]
+    label = "crack" if pred > 0.5 else "no_crack"
+
+    return {
+        "prediction": label,
+        "confidence": float(pred)
+    }
+@app.post("/predict_water_quality")
+async def predict_water_quality(data: dict):
+
+    features = np.array([[
+        data["ph"],
+        data["Hardness"],
+        data["Solids"],
+        data["Chloramines"],
+        data["Sulfate"],
+        data["Conductivity"],
+        data["Organic_carbon"],
+        data["Trihalomethanes"],
+        data["Turbidity"]
+    ]])
+
+    features = scaler.transform(features)  
+
+    pred = water_quality_model.predict(features)[0][0]
+
+    return {
+        "prediction": "potable" if pred > 0.5 else "not_potable",
+        "confidence": float(pred)
+    }
